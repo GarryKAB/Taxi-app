cb7428c53f0ec970ccfcfe8ac47f14537d400f10
Garry <garrykabongogilbert@gmail.com>
2019-05-09 07:53:02 +0200

    Added MapView to display map. GMaps API connected.

--- src/routes/Home/components/Home.js
+++ src/routes/Home/components/Home.js
@@ -1,17 +1,29 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
 
+import {Container} from 'native-base';
+import MapContainer from './MapContainer';
+
 class Home extends Component {
 
     componentDidMount(){
-        this.props.setName();
+        this.props.getCurrentLocation();
     }
 
     render(){
+        const region = {
+            latitude: -26.149444,
+            longitude: 28.101944,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421
+        }
         return (
-            <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-                <Text>Hello {this.props.name}</Text>
-            </View>
+            // <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            //     <Text>Hello {this.props.name}</Text>
+            // </View>
+            <Container>
+                <MapContainer region={region}/>
+            </Container>
         );
     }
 }
